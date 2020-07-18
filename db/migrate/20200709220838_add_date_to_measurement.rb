1197d57d63de6ccc4c95f9e9558b8cebca540776
manci <javiercaliescali@gmail.com>
2020-07-17 22:18:24 -0500

    check linters

--- db/migrate/20200709220838_add_date_to_measurement.rb
+++ db/migrate/20200709220838_add_date_to_measurement.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class AddDateToMeasurement < ActiveRecord::Migration[6.0]
   def change
     add_column :measurements, :date, :datetime
--- db/migrate/20200717124735_add_two_days_ago_to_progresses.rb
+++ db/migrate/20200717124735_add_two_days_ago_to_progresses.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class AddTwoDaysAgoToProgresses < ActiveRecord::Migration[6.0]
   def change
     add_column :progresses, :twodaysago, :string
